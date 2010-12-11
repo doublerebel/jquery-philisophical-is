4e611d1d97ca457ecf5da8c71852e5ecd8819637
doublerebel <charles@doublerebel.com>
2010-12-10 16:15:47 -0800

    Space, semicolon

--- jquery-philisophical-is.js
+++ jquery-philisophical-is.js
@@ -1,1 +1,1 @@
-(function($) { $.fn.extend({ _is: $.fn.is, is: function(s) { return s ? this._is(s) : !!this.length;} }); })(jQuery)
+(function($) { $.fn.extend({ _is: $.fn.is, is: function(s) { return s ? this._is(s) : !!this.length; } }); })(jQuery);
