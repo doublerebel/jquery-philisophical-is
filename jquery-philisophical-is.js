0d4353c02bd5f46940771b53517c6ae6936debfe
doublerebel <charles@doublerebel.com>
2010-12-10 16:02:04 -0800

    Performance optimizations, newlines at EOF

--- jquery-philisophical-is.js
+++ jquery-philisophical-is.js
@@ -1,1 +1,1 @@
-(function($) { $.fn.extend({ _is: $.fn.is, is: function(s) { return this._is(s || '*');} }); })(jQuery)
+(function($) { $.fn.extend({ _is: $.fn.is, is: function(s) { return s ? this._is(s) : !!this.length;} }); })(jQuery)
